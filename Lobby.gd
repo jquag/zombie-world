61fd45e6e09284906536ffa9ac42f6167485a56d
Sung Kang <sk5156@gmail.com>
2021-10-08 15:01:41 -0500

    Host only sets player offsets

--- Lobby.gd
+++ Lobby.gd
@@ -102,9 +102,14 @@ remote func pre_configure_game():
 		var player = preload("res://zombie/zombie.tscn").instance()
 		player.set_name(str(p))
 		player.set_network_master(p) # Will be explained later
-		player.position.x = player.position.x + 300
 		get_node("/root/World/Players").add_child(player)
 
+	
+	if get_tree().is_network_server():
+		var offset = 0
+		for p in get_node("/root/World/Players").get_children():
+			p.position.x = p.position.x + offset
+			offset = offset +  300
 	# Tell server (remember, server is always ID=1) that this peer is done pre-configuring.
 	# The server can call get_tree().get_rpc_sender_id() to find out who said they were done.
 	# rpc_id(1, "done_preconfiguring")
