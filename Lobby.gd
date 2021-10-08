6dac09222e38a3650da1177c6c704afdc9ebd2b5
John Quagliata <john.quagliata@gmail.com>
2021-10-08 14:37:01 -0500

    x

--- Lobby.gd
+++ Lobby.gd
@@ -102,8 +102,8 @@ remote func pre_configure_game():
 		var player = preload("res://zombie/zombie.tscn").instance()
 		player.set_name(str(p))
 		player.set_network_master(p) # Will be explained later
-		get_node("/root/World/Players").add_child(player)
 		player.position.x = player.position.x + 100
+		get_node("/root/World/Players").add_child(player)
 
 	# Tell server (remember, server is always ID=1) that this peer is done pre-configuring.
 	# The server can call get_tree().get_rpc_sender_id() to find out who said they were done.
